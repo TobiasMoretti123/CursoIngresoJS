f516fb4ff7e24e9b107f1c1b822c0f4e5e2f5269
unknown <tobiasmoretti14@gmail.com>
2021-02-02 15:06:16 -0300

    CursoIngresoJS

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,85 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	var numero1;
+
+	var numero2;
+	var suma;
+	var numero1parsiado
+	var numero2parsiado
+
+	numero1 = txtIdNumeroUno.value;
+	numero2 = txtIdNumeroDos.value;
+
+	//transfromacion
+	numero1parsiado = parseInt(numero1);
+	numero2parsiado = parseInt(numero2);
+
+	suma = numero1parsiado + numero2parsiado;
+
+	alert ("La suma es "+suma);	
 }
 
 function restar()
 {
-	
+	var numero1;
+
+	var numero2;
+	var suma;
+	var numero1parsiado
+	var numero2parsiado
+
+	numero1 = txtIdNumeroUno.value;
+	numero2 = txtIdNumeroDos.value;
+
+	//transfromacion
+	numero1parsiado = parseInt(numero1);
+	numero2parsiado = parseInt(numero2);
+
+	suma = numero1parsiado - numero2parsiado;
+
+	alert ("La resta es "+suma);
 }
 
 function multiplicar()
 { 
-	
+	var numero1;
+
+	var numero2;
+	var suma;
+	var numero1parsiado
+	var numero2parsiado
+
+	numero1 = txtIdNumeroUno.value;
+	numero2 = txtIdNumeroDos.value;
+
+	//transfromacion
+	numero1parsiado = parseInt(numero1);
+	numero2parsiado = parseInt(numero2);
+
+	suma = numero1parsiado * numero2parsiado;
+
+	alert ("La multiplicacion es "+suma);
 }
 
 function dividir()
 {
-	
+	var numero1;
+
+	var numero2;
+	var suma;
+	var numero1parsiado
+	var numero2parsiado
+
+	numero1 = txtIdNumeroUno.value;
+	numero2 = txtIdNumeroDos.value;
+
+	//transfromacion
+	numero1parsiado = parseInt(numero1);
+	numero2parsiado = parseInt(numero2);
+
+	suma = numero1parsiado / numero2parsiado;
+
+	alert ("La division es "+suma);
 }
 
