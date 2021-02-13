e3c5c6077feb3e6414bb769a97f45655b82a8a2a
TobiasMoretti123 <78236685+TobiasMoretti123@users.noreply.github.com>
2021-02-13 00:57:10 -0300

    CursoIngresoJS

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,20 @@
+/*Al seleccionar un destino informar si 
+hace FRIO o CALOR en ese destino*/
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	var destinoIngresado; 
+	
+	destinoIngresado = txtIdDestino.value;
 
-}//FIN DE LA FUNCIÓN
+	switch (destinoIngresado) 
+	{
+		case "Bariloche":
+		case "Ushuaia":
+			alert ("En este destino hace frio");
+			break;
+		case "Mar del plata":
+		case "Cataratas":
+			alert ("En este destino hace calor");
+			break;
+	}
+}
