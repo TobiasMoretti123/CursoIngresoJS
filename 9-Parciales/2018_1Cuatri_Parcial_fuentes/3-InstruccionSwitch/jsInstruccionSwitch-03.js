6fbd6b61872b327fb28664d8bc43bb66c2aede66
TobiasMoretti123 <78236685+TobiasMoretti123@users.noreply.github.com>
2021-02-19 18:26:21 -0300

    CursoIngresoJS

---
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -0,0 +1,18 @@
+/*al seleccionar un mes informar.
+si es Febrero: " Este mes no tiene más de 29 días."
+si NO es Febrero: "Este mes tiene 30 o más días"*/
+function mostrar()
+{
+	var mesDelAño;
+
+	mesDelAño = txtIdMes.value;
+	switch (mesDelAño) 
+	{
+		case("Febrero"):
+			alert("Este mes no tiene más de 29 días.")
+			break;
+		default:
+			alert ("Este mes tiene 30 o más días")
+			break;
+	}
+}
