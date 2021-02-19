3a162754460fc16b189c3e52988a5ab0bc8b47a0
TobiasMoretti123 <78236685+TobiasMoretti123@users.noreply.github.com>
2021-02-19 18:29:57 -0300

    Ingreso 2020

---
+++ 2018_1Cuatri_Parcial_fuentes/tres.js
@@ -0,0 +1,18 @@
+function mostrar()
+{
+    var precio;
+    var descuento;
+    var precioFinal;
+    var precioParceado;
+    var descuentoParceado;
+
+    precio = prompt ("Ingrese el Importe ");
+    descuento = prompt ("ingrese el descuento");
+
+    precioParceado = parseInt (precio);
+    descuentoParceado = parseInt (descuento);
+    precioFinal = precio-descuento;
+
+    elPrecioFinal.value = precioFinal;
+   
+}
